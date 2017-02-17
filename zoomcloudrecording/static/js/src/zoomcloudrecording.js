8ef45efabcefbe93e9284f9e9b32d37032358965
yasir1brahim <yasir.ibrahim@gmail.com>
2017-02-17 11:46:41 +0000

    apply dataTable only if table is not empty

--- zoomcloudrecording/static/js/src/zoomcloudrecording.js
+++ zoomcloudrecording/static/js/src/zoomcloudrecording.js
@@ -1,14 +1,18 @@
 /* Javascript for ZoomCloudRecordingBlock. */
 function ZoomCloudRecordingBlock(runtime, element) {
     try {
-        $(element).find('#meeting_table').dataTable({
-            "aoColumnDefs": [{ "bSortable": false, "aTargets": [ 0, 1, 2, 3 ] },
-                             { "bSearchable": false, "aTargets": [ 0, 1, 2, 3 ] }],
-            "bFilter": false,
-            "bLengthChange": false,
-            "sPaginationType": "full_numbers",
-            "iDisplayLength": {page_size}
-        });
+        var meeting_table = $(element).find('#meeting_table');
+        // apply dataTable only if there are valid records
+        if (meeting_table.find('tr').last().find('td')[0].innerHTML != "No Recordings") {
+            meeting_table.dataTable({
+                "aoColumnDefs": [{ "bSortable": false, "aTargets": [ 0, 1, 2, 3 ] },
+                                 { "bSearchable": false, "aTargets": [ 0, 1, 2, 3 ] }],
+                "bFilter": false,
+                "bLengthChange": false,
+                "sPaginationType": "full_numbers",
+                "iDisplayLength": {page_size}
+            });
+	}
     }
     catch(err) {}
 }
